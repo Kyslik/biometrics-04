87bf5ca3767bc3745ff26c15fac49a782f28e0a6
Martin Kiesel <martin.kiesel@gmail.com>
2016-04-23 18:30:22 +0200

    euclidean distance test

--- biometrics-04/main.cpp
+++ biometrics-04/main.cpp
@@ -7,14 +7,40 @@
 //
 
 #include <iostream>
+#include <ctime>
+
 #include "pca.hpp"
+#include "euclidean_distance.hpp"
 
 const biometrics_4::types::uintf TRAIN_SIZE = 10;
 const biometrics_4::types::uintf TEST_SIZE = 2;
 
 int main(int argc, const char * argv[])
 {
-    biometrics_4::pca::Pca p(TRAIN_SIZE, TEST_SIZE);
+    using namespace biometrics_4;
+    using namespace classifier;
+    std::vector<double> euclids;
+
+    biometrics_4::pca::Pca pca(TRAIN_SIZE, TEST_SIZE);
+    const types::PcaData &pca_data = pca.getPcaData();
+
+    euclid::EuclidDistance euclid(&pca_data);
+
+    size_t start = 0, end = 0;
+
+    for (types::uintf i = 1; i <= 60; i++)
+    {
+        start = clock();
+        pca.setComponents(i);
+        end = clock();
+        std::cout << (end - start) / (double)(CLOCKS_PER_SEC / 1000) << " ms" << std::endl;
+        //euclids.push_back(euclid.classify());
+    }
+
+//    for (const auto &e : euclids)
+//    {
+//        std::cout << e << std::endl;
+//    }
 
     return 0;
 }
