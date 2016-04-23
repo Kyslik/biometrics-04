3616fb24beaae1000600f744c3caa8903e0f5483
Martin Kiesel <martin.kiesel@gmail.com>
2016-04-23 18:31:15 +0200

    svm classifier structure

---
+++ biometrics-04/src/classifiers/svm.cpp
@@ -0,0 +1,9 @@
+//
+//  svm.cpp
+//  biometrics-04
+//
+//  Created by Martin Kiesel on 23/04/16.
+//  Copyright © 2016 Martin Kiesel. All rights reserved.
+//
+
+#include "svm.hpp"
---
+++ biometrics-04/src/classifiers/svm.hpp
@@ -0,0 +1,28 @@
+//
+//  svm.hpp
+//  biometrics-04
+//
+//  Created by Martin Kiesel on 23/04/16.
+//  Copyright © 2016 Martin Kiesel. All rights reserved.
+//
+
+#ifndef svm_hpp
+#define svm_hpp
+
+#include "biometrics_types.hpp"
+
+namespace biometrics_4
+{
+    namespace classifier
+    {
+        namespace svm
+        {
+            class Svm
+            {
+                
+            };
+        }
+    }
+}
+
+#endif /* svm_hpp */
